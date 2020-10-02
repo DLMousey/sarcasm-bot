3ecb657866a368a3077045ffc63504e46643620e
Toby Archer <me@tobbbl.es>
2020-10-02 19:39:46 +0200

    Introduce Linting (#6)
    * Introduce GitHub Actions + ESLint
    
    * overwrite dumb eslint rule to have a space after function name declaration

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+    'space-before-function-paren': ['error', 'never']
+  }
+}
--- bot.js
+++ bot.js
@@ -1,97 +1,94 @@
-const Discord = require("discord.js");
-const auth = require("./auth.json");
-const botConfig = require("./bot.config.json");
-const client = new Discord.Client();
+const Discord = require('discord.js')
+const auth = require('./auth.json')
+const botConfig = require('./bot.config.json')
+const client = new Discord.Client()
 
-client.on("ready", () => console.log("Signed in, Ready to rock and roll"));
+client.on('ready', () => console.log('Signed in, Ready to rock and roll'))
 
-client.on("message", (message) => {
-    if(!message.content.startsWith(botConfig.prefix) || message.author.bot) 
-        return;
+client.on('message', (message) => {
+  if (!message.content.startsWith(botConfig.prefix) || message.author.bot) { return }
 
-    const args = message.content.slice(botConfig.prefix.length).trim().split(/ +/g);
-    const command = args.shift().toLowerCase();
+  const args = message.content.slice(botConfig.prefix.length).trim().split(/ +/g)
+  const command = args.shift().toLowerCase()
 
-    switch (command) {
-        case 'mock':
-            mock(message, args);
-            break;
-        case 'clap':
-            clap(message, args);
-            break;
-        default:
-            return;
-    }    
-});
+  switch (command) {
+    case 'mock':
+      mock(message, args)
+      break
+    case 'clap':
+      clap(message, args)
+      break
+    default:
+  }
+})
 
 function mock(message, args) {
-    message.channel.fetchMessages()
+  message.channel.fetchMessages()
     .then(messages => {
-        let filteredMessages;
-        try {
-            filteredMessages = messages.filter(m => m.author.id === getUserId(args));
-        } catch (err) {
-            message.channel.send(`${message.author} ${err.message}`);
-            return;
+      let filteredMessages
+      try {
+        filteredMessages = messages.filter(m => m.author.id === getUserId(args))
+      } catch (err) {
+        message.channel.send(`${message.author} ${err.message}`)
+        return
+      }
+
+      const lastMessage = filteredMessages.first()
+      const newMessageParts = []
+      for (let i = 0; i < lastMessage.content.length; i++) {
+        if (lastMessage.content[i] === ' ') {
+          newMessageParts.push(' ')
+          continue
         }
 
-        const lastMessage = filteredMessages.first();
-        const newMessageParts = [];
-        for (let i = 0; i < lastMessage.content.length; i++) {
-            if (lastMessage.content[i] === ' ') {
-                newMessageParts.push(' ');
-                continue;
-            }
-
-            if (i % 2 === 0) {
-                newMessageParts.push(lastMessage.content[i].toUpperCase());
-            } else {
-                newMessageParts.push(lastMessage.content[i].toLowerCase());
-            }
+        if (i % 2 === 0) {
+          newMessageParts.push(lastMessage.content[i].toUpperCase())
+        } else {
+          newMessageParts.push(lastMessage.content[i].toLowerCase())
         }
+      }
 
-        message.channel.send(lastMessage.author + ' ' + newMessageParts.join(''));
-    });
+      message.channel.send(lastMessage.author + ' ' + newMessageParts.join(''))
+    })
 }
 
 function clap(message, args) {
-    message.channel.fetchMessages()
+  message.channel.fetchMessages()
     .then(messages => {
-        let filteredMessages;
-        try {
-            filteredMessages = messages.filter(m => m.author.id === getUserId(args));
-        } catch (err) {
-            message.channel.send(`${message.author} ${err.message}`);
-            return;
+      let filteredMessages
+      try {
+        filteredMessages = messages.filter(m => m.author.id === getUserId(args))
+      } catch (err) {
+        message.channel.send(`${message.author} ${err.message}`)
+        return
+      }
+
+      const lastMessage = filteredMessages.first()
+      const newMessageParts = []
+      for (let i = 0; i < lastMessage.content.length; i++) {
+        if (lastMessage.content[i] === ' ') {
+          newMessageParts.push('👏')
+          continue
         }
 
-        const lastMessage = filteredMessages.first();
-        const newMessageParts = [];
-        for (let i = 0; i < lastMessage.content.length; i++) {
-            if (lastMessage.content[i] === ' ') {
-                newMessageParts.push('👏');
-                continue;
-            }
-            
-                newMessageParts.push(lastMessage.content[i].toUpperCase());
-            
-            }
+        newMessageParts.push(lastMessage.content[i].toUpperCase())
+      }
 
-        message.channel.send(lastMessage.author + ' ' + newMessageParts.join(''));
-    });
+      message.channel.send(lastMessage.author + ' ' + newMessageParts.join(''))
+    })
 }
 
 const getUserId = (args) => {
-    let userId = null;
-    if (args.length) {
-        userId = args[0].replace(/[^0-9]/g, '');
-    }
+  let userId = null
+  if (args.length) {
+    userId = args[0].replace(/[^0-9]/g, '')
+  }
 
-    if (!userId) {
-        throw new Error('No user id, or invalid user id provided');
-    }
+  if (!userId) {
+    throw new Error('No user id, or invalid user id provided')
+  }
 
-    return userId;
+  return userId
 }
 
-client.login(auth.token);
+client.login(auth.token)
